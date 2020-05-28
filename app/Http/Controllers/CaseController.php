206e6921a2858383db9e1fccaf65a3b4d0906f5a
LiNo <castlino@gmail.com>
2020-05-29 01:06:27 +1000

    Added pagination

--- app/Http/Controllers/CaseController.php
+++ app/Http/Controllers/CaseController.php
@@ -13,9 +13,34 @@ class CaseController extends Controller
     }
     
     public function casesPaginated(){
-          $count = request('count') ? request('count') : 2;
+          $count = request('count') ? (int) request('count') : 2;
+          $page = request('page') ? (int) request('page') : 1;
+          $startFrom = ($page - 1) * $count;
           
-          $cases = DB::table('cases')->take($count)->get();
-          return response()->json($cases);
+          $total = DB::table('cases')->count();
+          
+          $end = $startFrom + $count;
+          if($end > $total){
+            $end = $total;
+          }
+          
+          $maxPage = intdiv($total, $count);
+          if(fmod($total, $count) > 0){
+            $maxPage++;
+          }
+          
+          
+          $cases = DB::table('cases')->skip($startFrom)->take($count)->get();
+          return response()->json(
+            [
+              'count' => $count,
+              'page' => $page,
+              'start' => $startFrom + 1,
+              'end' => $end,
+              'total' => $total,
+              'maxPage' => $maxPage,
+              'cases' => $cases
+            ]
+          );
     }
 }
